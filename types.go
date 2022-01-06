7949f88cb861d15232cd3898e54f907d4e900cac
jonreesman <jreesman1@mac.com>
2022-01-06 14:00:49 +0000

    Modified types to support DynamoDB support.

--- types.go
+++ types.go
@@ -1,12 +1,18 @@
 package main
 
+import "time"
+
 type ticker struct {
 	name            string
+	lastScrapeTime	time.Time
 	numTweets       int
 	tweets          []statement
 	hourlySentiment float64
 }
 type statement struct {
-	expression string
-	polarity   uint8
+	expression string 	`dynamodbav="expression"`
+	subject string		`dynamodbav="subject"`
+	source string		`dynamodbav="source"`
+	timeStamp int64		`dynamodbav="timestamp"`
+	polarity   uint8	`dynamodbav="polarity"`
 }
